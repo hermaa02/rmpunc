9d837d38c7918a4ca8bac2e9a7d4afa0424cbb35
hermaa02 <hermaa02@8264.local>
2015-10-20 13:30:10 -0500

    Initial commit

---
+++ ispunc.h
@@ -0,0 +1,5 @@
+#ifndef ispunc_h
+#define ispunc_h
+int ispunc(char c);
+
+#endif
---
+++ rmpunc.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include "ispunc.h"
+
+int main(int argc, char* argv[]) {
+
+	char c ;
+	
+/*	c = getchar();
+
+	while (c != EOF) {
+		if (!ispunc(c))
+			printf("%c",c);
+		c = getchar();
+
+	}
+*/
+
+	while ((c=getchar()) != EOF) {
+		if (!ispunc(c))
+			printf("%c",c);
+	
+	}	
+	return 0;
+}
